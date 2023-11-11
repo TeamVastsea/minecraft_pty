b30c3c48f72c7271add84f9de4b5c75be6ecf196
Snowball_233 <wangrb.qq@outlook.com>
2023-11-12 00:13:46 +0800

    feat: frontend demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// use std::process::{Command, Stdio};
+//
+// fn main() {
+//     let mut child = Command::new("cmd")
+//         .stdin(Stdio::piped())
+//         .stdout(Stdio::piped())
+//         .spawn()
+//         .expect("err");
+//     let mut child_stdin = child.stdin.take().expect("stdin err");
+//     //不会咯（？）
+// }
