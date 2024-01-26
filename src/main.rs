238f49b79a01bf40ad068ff8d4b3f6824778f544
Snowball_233 <wangrb.qq@outlook.com>
2024-01-26 21:46:50 +0800

    Revert "feat: pty"
    This reverts commit cd9c06471905019b85084ba7c96c44b029ef1435.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
-mod process;
-
-use tokio::spawn;
-use tokio::task::JoinHandle;
-use crate::process::process::{InputPacket, run};
-
-#[tokio::main]
-async fn main() {
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
-    let (tx2, mut rx2) = tokio::sync::mpsc::channel(100);
-
-    let h1: JoinHandle<()> = spawn(async move {
-        run(
-            "C:\\Program Files\\Java\\jdk-17.0.2\\bin\\java.exe",
-            vec!["-jar", "./paper.jar", "nogui"],
-            "./program/",
-            tx,
-            rx2,
-        ).await;
-    });
-
-    let h2: JoinHandle<()> = spawn(async move {
-        loop {
-            let mut s = String::new();
-            std::io::stdin().read_line(&mut s).unwrap();
-            let packet = InputPacket {
-                input_type: crate::process::process::InputType::Command,
-                message: s,
-            };
-            tx2.send(packet).await.unwrap();
-        }
-    });
-
-    let h3: JoinHandle<()> = spawn(async move {
-        while let Some(a) = rx.recv().await {
-            println!("{}", serde_json::to_string_pretty(&a).unwrap());
-        }
-    });
-
-    tokio::try_join!(h1, h2, h3).unwrap();
+fn main() {
+    println!("Hello, world!");
 }
 
+// use std::process::{Command, Stdio};
+//
+// fn main() {
+//     let mut child = Command::new("cmd")
+//         .stdin(Stdio::piped())
+//         .stdout(Stdio::piped())
+//         .spawn()
+//         .expect("err");
+//     let mut child_stdin = child.stdin.take().expect("stdin err");
+//     //不会咯（？）
+// }
